d0ccb95c8a75f1c8e412d4fa988430694272b685
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-07-10 07:33:10 +0200

    add cloudinary widget

--- src/components/settings/SettingsLogoSection.vue
+++ src/components/settings/SettingsLogoSection.vue
@@ -1,11 +1,29 @@
+<script setup lang="ts">
+const widget = window?.cloudinary.createUploadWidget(
+  {
+    cloudName: import.meta.env.VITE_GLOB_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: import.meta.env.VITE_GLOB_CLOUDINARY_UPLOAD_PRESET
+  },
+  (error, result) => {
+    if (!error && result && result.event === 'success') {
+      console.log('Done! Here is the image info: ', result.info)
+    }
+  }
+)
+
+const handleOpenUploadWidget = () => widget.open()
+</script>
+
 <template>
   <v-row class="my-2">
     <v-col>
       <h2>Change footer content</h2>
     </v-col>
     <v-col class="d-flex flex-column align-end">
-      <v-btn class="mb-2" width="150" color="primary">Upload Image</v-btn>
-      <v-btn width="150" color="primary">Enter HTML</v-btn>
+      <v-btn class="mb-2" width="150" color="primary" @click="handleOpenUploadWidget"
+        >Upload Image</v-btn
+      >
+      <v-btn width="150" color="primary">Insert HTML</v-btn>
     </v-col>
   </v-row>
 </template>
